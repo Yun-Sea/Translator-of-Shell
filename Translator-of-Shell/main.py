0406748ddc8febdfeab5fb7a403d5da8109fd152
Xie Pengfei <xpf.acc@outlook.com>
2025-06-11 18:44:27 +0800

    细节优化

--- Translator-of-Shell/main.py
+++ Translator-of-Shell/main.py
@@ -9,7 +9,7 @@ def transition(input_str: str) -> bs4.BeautifulSoup | None:
     :param input_str:str
     :return content:bs4.element.Tag
     """
-    print("正在尝试连接翻译网站...")
+    print("正在尝试连接翻译网站==>", end=' ')
     url = f"https://dict.youdao.com/result?word={input_str}&lang=en"
     try:
         r = requests.get(url)
@@ -30,26 +30,38 @@ def is_valid_content(input_str, check_content) -> bs4.element.Tag | None:
     """
     content = check_content.text
     if content == "":
+        # 无相关词汇
         print(f"抱歉，没有找到「{input_str}」相关的词")
         return None
     elif check_content.find('div', class_='maybe'):
+        # 有可能的词汇
         maybe_div = check_content.find('div', class_='maybe')
+        print("你要找的是不是:")
         for word_div in maybe_div.find_all('div', class_='maybe_word'):
             word = word_div.find('a', class_='point').get_text(strip=True)
             trans = word_div.find('p', class_='maybe_trans').get_text(strip=True)
-            print(f"  {word}: {trans}")
+            if trans == "":
+                print(f"  {word}")
+            else:
+                print(f"  {word} : {trans}")
         return None
     else:
+        # 正常内容
         return check_content
 
 
 def content_parse(content: bs4.element.Tag):
+    """
+    解析查询的基本内容
+    :param content:
+    :return:
+    """
     # 解析查询的内容标题
     title = content.find('div', class_='word-head').find_all('div')[0].contents[0]
-    print("标题", title)
+    print("\n翻译内容:", title)
 
     try:
-        # 英文翻译中文
+        # 尝试英文翻译中文
 
         # 发音(如果有)
         pronounce = content.find('div', class_='phone_con')
@@ -58,14 +70,14 @@ def content_parse(content: bs4.element.Tag):
 
         # 基本释义
         basics = content.find('ul', class_='basic').find_all('li')
-        print("基本释义：")
+        print("\n基本释义：")
         for basic in basics:
-            print(basic.text)
+            print(basic.text[0] + " : " + basic.text[1:])
 
-    except Exception as e:
-        # 中文翻译英文
-        f = content.find_all('div', class_='trans-container')[1].contents[0]
-        print(f.text)
+    except AttributeError as e:
+        # 基本翻译
+        basic_str = content.find_all('div', class_='trans-container')[1].contents[0]
+        print("\n基本翻译：\n", basic_str.text)
 
 
 def content_parse_more(content: bs4.element.Tag):
@@ -74,11 +86,12 @@ def content_parse_more(content: bs4.element.Tag):
     :param content:
     :return:
     """
-
-    webPhrases = content.find('div', class_='webPhrase').find_all('li')
-    for webPhrase in webPhrases:
+    print("\n网络短语：")
+    web_phrases = content.find('div', class_='webPhrase').find_all('li')
+    for webPhrase in web_phrases:
         print(webPhrase.text)
 
+    print("\n双语例句：")
     blng = content.find('div', class_='blng_sents_part').find_all('li')
     for bln in blng:
         print(bln.text)
@@ -106,15 +119,23 @@ def main():
 
     args = parser.parse_args()
 
-    # 根据参数执行操作
+    # 获取命令行单词
     word = args.query
+
+    # 开始转换对象
     t_str = transition(word)
     if t_str is None:
         print("转换失败！")
-        return
+        return 0
+
+    # 判断内容
     if is_valid_content(word, t_str) is None:
-        return
+        return 0
+
+    # 解析内容
     content_parse(t_str)
+
+    # -a 参数 , 展示更多内容
     if args.all:
         content_parse_more(t_str)
 
